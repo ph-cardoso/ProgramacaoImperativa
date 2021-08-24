65eeecfb69fa98521154796a91f2ddea52960581
Pedro Cardoso <phlc.cardoso@gmail.com>
2021-08-24 10:29:18 -0300

    update

--- aula08/mesa_de_trabalho.js
+++ aula08/mesa_de_trabalho.js
@@ -1,19 +1,23 @@
-//* Function podeSubir()
-function podeSubir(altura, acompanhada)
-{
-    if(altura >= 1.4 && altura < 2)
+// Função
+function podeSubir(altura, seAcompanhado){
+    if(altura >= 1.4 && altura < 2.0){
         console.log(`Acesso autorizado!`);
-    else if(altura > 1.2 && altura < 1.4)
-        acompanhada ? console.log(`Acesso autorizado com acompanhante!`) : console.log(`Acesso não autorizado por ausência de acompanhante!`);
-    else
-        console.log(`Acesso não autorizado!`);
+        return true;
+    }
+    else if(altura < 1.4 && altura >= 1.2 && seAcompanhado){
+        console.log(`Acesso autorizado somente com acompanhante!`);
+        return true;
+    }
+    else{
+        console.log(`Acesso negado!`);
+        return false;
+    }
 }
 
-
-//* Teste da função podeSubir()
-podeSubir(1.4, true);   // Expected: "Acesso autorizado!"
-podeSubir(1.5, false);  // Expected: "Acesso autorizado!"
-podeSubir(1.3, true);   // Expected: "Acesso autorizado com acompanhante!"
-podeSubir(1.3, false);  // Expected: "Acesso não autorizado por ausência de acompanhante!"
-podeSubir(1.1, true);   // Expected: "Acesso não autorizado"
-podeSubir(1.1, false);  // Expected: "Acesso não autorizado"
+// Teste da função podeSubir()
+podeSubir(1.4, true);       // Expected: "Acesso autorizado!"
+podeSubir(1.5, false);      // Expected: "Acesso autorizado!"
+podeSubir(1.3, true);       // Expected: "Acesso autorizado com acompanhante!"
+podeSubir(1.35, false);     // Expected: "Acesso não autorizado por ausência de acompanhante!"
+podeSubir(1.1, true);       // Expected: "Acesso não autorizado"
+podeSubir(1.0, false);      // Expected: "Acesso não autorizado"
